d15ad1deb9414907cfaaadfbc7877f6d2cb35613
Chong <aaa59891@gmail.com>
2019-10-28 23:39:13 +0800

    use ListTile

--- lib/widgets/stateless/transaction_item.dart
+++ lib/widgets/stateless/transaction_item.dart
@@ -8,44 +8,23 @@ class TransactionItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Theme.of(context).primaryColor,
-                width: 2,
-              ),
-            ),
-            padding: EdgeInsets.all(5),
+      elevation: 5,
+      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: FittedBox(
             child: Text(
               "\$${transaction.amount.toStringAsPrecision(2)}",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Theme.of(context).primaryColor,
-              ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                transaction.title,
-                style: Theme.of(context).textTheme.title,
-              ),
-              Text(
-                DateFormat.yMMMd().format(transaction.date),
-                style: TextStyle(color: Colors.grey),
-              ),
-            ],
-          )
-        ],
+        ),
+        title: Text(
+          this.transaction.title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        subtitle: Text(
+          DateFormat.yMMMd().format(transaction.date),
+        ),
       ),
     );
   }
