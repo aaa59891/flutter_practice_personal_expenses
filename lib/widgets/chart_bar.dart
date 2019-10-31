0fd3e762ee688bc00cd0a128b6165683d84e9195
Chong <aaa59891@gmail.com>
2019-10-31 23:32:12 +0800

    use LayoutBuilder to set height

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -6,40 +6,52 @@ class ChartBar extends StatelessWidget {
   ChartBar(this.txData);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(
-          this.txData.amount.toStringAsFixed(2),
-          overflow: TextOverflow.ellipsis,
-        ),
-        SizedBox(height: 4),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return Column(
+          children: <Widget>[
+            Container(
+              height: constraints.maxHeight * .15,
+              child: Text(
+                this.txData.amount.toStringAsFixed(2),
+                overflow: TextOverflow.ellipsis,
               ),
-              FractionallySizedBox(
-                heightFactor: this.txData.percentage,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10),
+            ),
+            SizedBox(height: constraints.maxHeight * .05),
+            Container(
+              height: constraints.maxHeight * .6,
+              width: 10,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                ),
+                  FractionallySizedBox(
+                    heightFactor: this.txData.percentage,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(height: 5),
-        Text(this.txData.dayLabel.substring(0, 1)),
-      ],
+            ),
+            SizedBox(
+              height: constraints.maxHeight * .05,
+            ),
+            Container(
+              height: constraints.maxHeight * .15,
+              child: Text(this.txData.dayLabel.substring(0, 1)),
+            ),
+          ],
+        );
+      },
     );
   }
 }
